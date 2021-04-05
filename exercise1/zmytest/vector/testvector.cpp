9023c11eac8794b0f69beb7b3c131631e340a2a5
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-04-05 12:31:31 +0200

    Aggiunte vettori double e string

--- exercise1/zmytest/vector/testvector.cpp
+++ exercise1/zmytest/vector/testvector.cpp
@@ -4,6 +4,10 @@
 #include "../../vector/vector.hpp"
 #include "../menu.hpp"
 #include <random>
+#include <string>
+#include <ctime>
+#include <unistd.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
@@ -15,25 +19,57 @@ void VectorInt(){
   unsigned long dim; // Dichiariamo una variabile per la scelta della dimensione del vettore
   int index;
 
-  cout<< "Inserire la dimensione del vettore: "<<endl;
-  cin >> dim;
+  std::cout<< "Inserire la dimensione del vettore: ";
+  std::cin >> dim;
   lasd::Vector<int> vec(dim); // Inizializziamo il vettore
   
   for(unsigned long i = 0; i < dim; i++) { // Riempimento del vettore
     vec[i] = dist(gen);
   }
-  cout << "FRONT: "<< vec.Front() << endl; // Stampa del Front del Vettore
-  cout << "FRONT: "<< vec.Back() << endl; // Stampa del Back del Vettore
+  std::cout << "Il Front del vettore è: "<< vec.Front() << std::endl; // Stampa del Front del Vettore
+  std::cout << "Il Back del vettore è: "<< vec.Back() << std::endl; // Stampa del Back del Vettore
+ 
+  // Ricerca dell'elemento di indice scelto dall'utente
+  std::cout << "Ricerca dell'elemento di indice scelto dall'utente ";
+  std::cin >> index;
+  while (index > dim - 1){
+    std::cout << "L'indice selezionato non è valido! Inseriscine uno valido: ";
+    std::cin >> index;
+  }
+
+  for (int i = 0 ; i < dim ; i ++){
+    if (index == i){
+      std::cout << "L'indice scelto " << index << " contiene il valore " << vec[i] << std::endl;
+    }
+  }
+  
+}
+
+void VectorDouble(){
+  default_random_engine gen(random_device{}()); 
+  uniform_real_distribution<double> dist(1000.000, 8000.000); // Generazione numeri casuali
+
+  unsigned long dim;
+  int index;
+  std::cout<< "Inserire la dimensione del vettore: ";
+  std::cin >> dim;
+  lasd::Vector<double> vec(dim); // Inizializziamo il vettore
+  
+  for(unsigned long i = 0; i < dim; i++) { // Riempimento del vettore
+    vec[i] = dist(gen);
+  }
+  std::cout << "Il Front del vettore è: "<< vec.Front() << std::endl; // Stampa del Front del Vettore
+  std::cout << "Il Back del vettore è: "<< vec.Back() << std::endl; // Stampa del Back del Vettore
 
   for(unsigned long i = 0; i < dim; i++) {
     cout << "VEC:" << vec[i] << endl;
   }
  
   // Ricerca dell'elemento di indice scelto dall'utente
-  cout << "Ricerca dell'elemento di indice scelto dall'utente " << endl;
+  cout << "Ricerca dell'elemento di indice scelto dall'utente ";
   cin >> index;
   while (index > dim - 1){
-    cout << "L'indice selezionato non è valido! " << endl;
+    cout << "L'indice selezionato non è valido! Inseriscine uno valido: ";
     cin >> index;
   }
 
@@ -45,12 +81,35 @@ void VectorInt(){
   
 }
 
-void VectorDouble(){
-  cout << "OK" << endl;
-  
-}
-
 void VectorString(){
-  cout << "Stringa" << endl;
-  
+  unsigned long dim;
+  int index;
+  std::cout << "Inserire la dimensione del vettore: ";
+  std::cin >> dim;
+  lasd::Vector<string> vec(dim); // Inizializziamo il vettore
+  std::size_t length;
+  std::mt19937 mt(std::random_device{}());
+  std::string characters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()";
+  std::uniform_int_distribution<int> dist(0, characters.length() - 1);
+  std::string randStr;
+
+  for (unsigned long i = 0; i < dim; i++) { // Riempimento del vettore
+    randStr = characters[dist(mt)];
+    vec[i] = randStr;
+  }
+  std::cout << "Il Front del vettore è: "<< vec.Front() << endl; // Stampa del Front del Vettore
+  std::cout << "Il Back del vettore è: "<< vec.Back() << endl; // Stampa del Back del Vettore
+  std::cout << "Ricerca dell'elemento di indice scelto dall'utente ";
+  std::cin >> index;
+  while (index > dim - 1){
+    std::cout << "L'indice selezionato non è valido! Inseriscine uno valido: ";
+    std::cin >> index;
+  }
+
+  for (int i = 0 ; i < dim ; i ++){
+    if (index == i){
+      std::cout << "L'indice scelto " << index << " contiene il valore " << vec[i] << std::endl;
+    }
+  }
 }
