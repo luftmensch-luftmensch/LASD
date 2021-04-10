332536fe2f49e51d4eed37ad42964ada40164022
anthonyjr7 <81435645+anthonyjr7@users.noreply.github.com>
2021-04-10 18:57:38 +0200

    Add files via upload

---
+++ LASD-main/exercise1/zmytest/vector/testvector.cpp
@@ -0,0 +1,213 @@
+/* INCLUSIONI */
+#include <iostream>
+#include "../../container/container.hpp"
+#include "../container/container.hpp"
+#include "../../vector/vector.hpp"
+#include "../menu.hpp"
+#include <random>
+#include <string>
+#include <ctime>
+#include <unistd.h>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/* Prototipi */
+void RecapMenu();
+void VectorInt(){
+  default_random_engine gen(random_device{}());
+  uniform_int_distribution<unsigned int> dist(1, 800); // Generazione numeri casuali
+
+  unsigned long dim; // Dichiariamo una variabile per la scelta della dimensione del vettore
+  int index, value;
+  int n;
+  int sum=0;
+  std::cout<< "Inserire la dimensione del vettore: ";
+  std::cin >> dim;
+  std::cout<< "\n";
+  lasd::Vector<int> vec(dim); // Inizializziamo il vettore
+
+  for(unsigned long i = 0; i < dim; i++) { // Riempimento del vettore
+    vec[i] = dist(gen);
+  }
+  GetFront(vec);
+  GetBack(vec);
+
+  // Ricerca dell'elemento di indice scelto dall'utente
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~Info sul contenuto dell'elemento di indice scelto dall'utente ~~~" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n\n" << std::endl;
+  std::cout << "Inserisci la posizione dell'elemento di cui ti interessa conoscere il valore: ";
+  std::cin >> index;
+  while (index > dim - 1){
+    std::cout << "L'indice selezionato non è valido! Inserirne uno valido: ";
+    std::cin >> index;
+  }
+
+  for (int i = 0 ; i < dim ; i ++){
+    if (index == i){
+      std::cout << "L'indice scelto " << index << " contiene il valore " << vec[i] << std::endl;
+    }
+  }
+
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~ Ricerca di un valore all'interno della struttura~~~\n\tInserisci elemento: ";
+  std::cin>> value;
+  Exists(vec, value);
+  std::cout << "\n" << std::endl;
+  MapPreOrder(vec, &MapPrint<int>, 0);
+  std::cout << "\n" << std::endl;
+
+  std::cout << "\nInserisci un valore per cui calcolare la somma degli elementi minori del valore dato: \n"<< std::endl;
+  std::cin>> n;
+  vec.FoldPreOrder([](const int& dat, const void* n, void* sum)
+	      {
+          if (dat < *((int*)n))
+            {
+              *((int*)sum) += dat;
+          }
+      }, &n, &sum);
+  std::cout << sum << std::endl;
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~ Applicazione della funzione 2N agli elementi del vettore~~~\n\t"<<std::endl;
+  MapPreOrder(vec, &MapDouble<int>,0);
+  std::cout << "\n" << std::endl;
+  MapPreOrder(vec, &MapPrint<int>, 0);
+  std::cout << "\n" << std::endl;
+  RecapMenu();
+}
+
+void VectorFloat(){
+  default_random_engine gen(random_device{}());
+  uniform_real_distribution<float> dist(1, 800); // Generazione numeri casuali
+
+  unsigned long dim;
+  int index;
+  float value;
+  float n;
+  float prod=1.0;
+  std::cout<< "Inserire la dimensione del vettore: ";
+  std::cin >> dim;
+  std::cout<< "\n";
+  lasd::Vector<float> vec(dim); // Inizializziamo il vettore
+
+  for(unsigned long i = 0; i < dim; i++) { // Riempimento del vettore
+    vec[i] = dist(gen);
+  }
+
+  GetFront(vec);
+  GetBack(vec);
+
+  // Ricerca dell'elemento di indice scelto dall'utente
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~Info sul contenuto dell'elemento di indice scelto dall'utente ~~~" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n\n" << std::endl;
+  std::cout << "Inserisci la posizione dell'elemento di cui ti interessa conoscere il valore: ";
+  cin >> index;
+  while (index > dim - 1){
+    cout << "L'indice selezionato non è valido! Inserirne uno valido: ";
+    cin >> index;
+  }
+
+  for (int i = 0 ; i < dim ; i ++){
+    if (index == i){
+      cout << "L'indice scelto " << index << " contiene il valore " << vec[i] << endl;
+    }
+  }
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~ Ricerca di un valore all'interno della struttura~~~\n\tInserisci elemento: ";
+  std::cin>> value;
+  Exists(vec, value);
+  std::cout << "\n" << std::endl;
+  MapPreOrder(vec, &MapPrint<float>, 0);
+  std::cout << "\n" << std::endl;
+  std::cout << "\nInserisci un valore per cui calcolare il prodotto degli elementi maggiori del valore dato: \n"<< std::endl;
+  std::cin>> n;
+  vec.FoldPreOrder([](const float& dat, const void* n, void* prod)
+	    {
+        if (dat > *((float*)n)) {
+          *((float*)prod) *= dat;
+        }
+}, &n, &prod);
+if (prod!=1.0){
+  std::cout << "Il prodotto e' "<<prod << std::endl;
+}
+else{
+  std::cout << "Non vi sono elementi maggiori dell'elemento scelto" << std::endl;
+}
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~ Applicazione della funzione (N)^2 agli elementi del vettore~~~\n\t"<<std::endl;
+  MapPreOrder(vec, &MapNxN<float>,0);
+  std::cout << "\n" << std::endl;
+  MapPreOrder(vec, &MapPrint<float>, 0);
+  std::cout << "\n" << std::endl;
+  RecapMenu();
+}
+
+void VectorString(){
+  unsigned long dim;
+  int index;
+  std::string value, StringaConcatenata;
+  int n;
+  std::cout << "Inserire la dimensione del vettore: ";
+  std::cin >> dim;
+  std::cout<< "\n";
+  lasd::Vector<string> vec(dim); // Inizializziamo il vettore
+  std::size_t length;
+  std::mt19937 mt(std::random_device{}());
+  std::string characters =
+      "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  std::uniform_int_distribution<int> dist(0, characters.length() - 1);
+  std::string randStr;
+
+  for (unsigned long i = 0; i < dim; i++) { // Riempimento del vettore
+    randStr = characters[dist(mt)];
+    vec[i] = randStr;
+  }
+
+  GetFront(vec);
+  GetBack(vec);
+
+  // Ricerca dell'elemento di indice scelto dall'utente
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~Info sul contenuto dell'elemento di indice scelto dall'utente ~~~" << std::endl;
+  std::cout << "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n\n\n\n\n" << std::endl;
+  std::cout << "Inserisci la posizione dell'elemento di cui ti interessa conoscere il valore: ";
+  std::cin >> index;
+  while (index > dim - 1){
+    std::cout << "L'indice selezionato non è valido! Inserirne uno valido: ";
+    std::cin >> index;
+  }
+
+  for (int i = 0 ; i < dim ; i ++){
+    if (index == i){
+      std::cout << "L'indice scelto " << index << " contiene il valore " << vec[i] << std::endl;
+    }
+  }
+  std::cout << "\n\n\n\n\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~" << std::endl;
+  std::cout << "~~~ Ricerca di un valore all'interno della struttura~~~\n\tInserisci elemento: ";
+  std::cin>> value;
+
+  Exists(vec, value);
+  std::cout << "\n" << std::endl;
+  MapPreOrder(vec, &MapPrint<std::string>, 0);
+  std::cout << "\n" << std::endl;
+  std::cout << "\nCalcolo della concatenazione di stringhe dato un valore n: \n"<< std::endl;
+  std::cin>> n;
+  vec.FoldPreOrder([](const std::string& dat, const void* n, void* StringaConcatenata)
+	      {
+                if (dat.length() < *((int*)n))
+                {
+                  *((std::string*)StringaConcatenata) += dat;
+                }
+              }, &n, &StringaConcatenata);
+  std::cout << StringaConcatenata << std::endl;
+
+  RecapMenu();
+}
+
+void RecapMenu(){
+  std::cout<<"Seleziona il tipo di struttura\n\tVettore di interi (1)\tVettore di Float(2)\tVettore di Stringhe(3)\n"<< std::endl;
+  std::cout<<"\tLista di interi (4)\tLista di Float(5)\tListe di Stringhe(6)\n"<< std::endl;
+  std::cout<<"\t\t\t(premi q per uscire)"<< std::endl;
+}
