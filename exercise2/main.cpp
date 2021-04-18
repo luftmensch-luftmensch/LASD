0520392be03dfaccb66a30c3737d4a049bc04a40
luftmensch-luftmensch <valentinobocchetti59@gmail.com>
2021-04-18 17:25:20 +0200

    Aggiunta menu

--- exercise2/main.cpp
+++ exercise2/main.cpp
@@ -1,4 +1,3 @@
-
 #include "zlasdtest/test.hpp"
 
 #include "zmytest/test.hpp"
@@ -10,7 +9,19 @@
 /* ************************************************************************** */
 
 int main() {
-  std::cout << "Lasd Libraries 2020" << std::endl;
-  lasdtest(); // To call in the menu of your library test!
+  std::cout << "Lasd Libraries 2021" << std::endl;
+  int scelta;
+  std::cout<<"Seleziona il tipo di test da eseguire:"<<std::endl;
+  std::cout<<"\tTest professore (1)\tTest Studente(2)"<<std::endl;
+  std::cin>>scelta;
+  if(scelta == 1){
+    std::cout<<""<<std::endl;
+    lasdtest(); // To call in the menu of your library test!
+  }
+  if(scelta == 2){
+    std::system("clear");
+    std::cout<<""<<std::endl;
+    mytest();
+  }
   return 0;
 }
--- exercise2/zmytest/test.cpp
+++ exercise2/zmytest/test.cpp
@@ -1,2 +1,85 @@
+#include "../zlasdtest/test.hpp"
 
-// ...
+#include "test.hpp"
+#include "StackVec/StackVec.hpp"
+
+/* ************************************************************************** */
+
+#include <iostream>
+
+/* ************************************************************************** */
+
+void mytest(){
+  bool quit = false;
+  char scelta;
+  std::cout << "Scegli il tipo di struttura: "<< std::endl;
+  std::cout<<"Stack implementato con vettori (1 -> int; 2 -> double; 3 -> string)"<< std::endl;
+  std::cout<<"Stack implementato con lista (4 -> int; 5 -> double; 6 -> string)"<< std::endl;
+  std::cout<<"Queue implementata con vettori (a -> int; b -> double; c -> string)"<< std::endl;
+  std::cout<<"Queue implementato con lista (d -> int; e -> double; f -> string)"<< std::endl;
+  std::cout<<"\t\t\t(premi q per uscire)"<< std::endl;
+
+  while (scelta != 'q'){
+    std::cin >> scelta;
+
+    if ((scelta != '1') && (scelta != '2') && (scelta != '3') &&
+	(scelta != '4') && (scelta != '5') && (scelta != '6') &&
+	(scelta != 'a') && (scelta != 'b') && (scelta != 'c') &&
+	(scelta != 'd') && (scelta != 'e') && (scelta != 'f') &&
+	(scelta != 'q')) {
+      std::cout << "ERRORE: scelta non valida\n";
+    }
+    if (scelta == '1') {
+      std::system("clear");
+      StackVecInt();
+    }
+    if (scelta == '2') {
+      std::system("clear");
+      StackVecDouble();
+      quit = true;
+    }
+
+    if (scelta == '3') {
+      std::system("clear");
+      StackVecString();
+      quit = true;
+    }
+    if (scelta == '4') {
+      std::system("clear");
+      quit = true;
+    }
+
+    if (scelta == '5') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == '6') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == 'a') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == 'b') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == 'c') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == 'd') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == 'e') {
+      std::system("clear");
+      quit = true;
+    }
+    if (scelta == 'f') {
+      std::system("clear");
+      quit = true;
+    }
+  }
+}
